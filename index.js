71e5d134d44d1bc1ecbb646b9a00cbcd18a6c8d6
Bryan Castanos <bcastanos@gmail.com>
2016-03-04 12:29:34 -0800

    added a contact route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
 
+app.get('/contact', function(req,res){
+  res.send("Contact page here.")
+})
+
 app.listen(3000, function(){
   console.log('listening on port 3000')
 })
