c31d136f9f5524d56c7c7ac38fea1702745a65a1
Laura Leonard <Laura.Leonard818@gmail.com>
2016-03-04 12:28:28 -0800

    added routes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
+var app = express()
+
+app.get('/', function(req,res){
+  res.send('Welcome Home')
+})
+
+app.get('/about', function(req,res){
+  res.send('About Page')
+})
 
 app.listen(3000, function(){
   console.log('listening on port 3000')
