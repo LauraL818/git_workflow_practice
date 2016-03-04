b7eb64cf65e617a4d978312ca1738485dceefcaa
Laura Leonard <Laura.Leonard818@gmail.com>
2016-03-04 12:25:49 -0800

    added app.listen

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
+
+app.listen(3000, function(){
+  console.log('listening on port 3000')
+})
